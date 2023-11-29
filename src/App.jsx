590720f6f5e00cb6a68fe0db431d842e522cb6d8
Kaj Białas <kaj.bialas@dev-industry.com>
2023-11-29 18:51:25 +0100

    Finish styles

--- src/App.jsx
+++ src/App.jsx
@@ -28,16 +28,16 @@ function App() {
         return (<div>Loader...</div>)
     }
   return (
-    <div>
-        <section>
+    <div className="min-h-screen bg-gray-900 text-white justify-center items-center flex-col m-auto">
+        <section className="bg-black text-white p-8 text-center">
             <h1 className="text-3xl font-bold mb-4">
                 Spotify
             </h1>
-            <img src={songs[selectedSong].cover} />
-            <button onClick={() => audioRef.current.play()}>
+            <img src={songs[selectedSong].cover} className="mx-auto my-4 w-64 object-cover shadow-lg"/>
+            <button className="bg-green-500 hover:bg-green-700 text-white py-2 px-4 rounded-full cursor-pointer" onClick={() => audioRef.current.play()}>
                 Play
             </button>
-            <button onClick={() => audioRef.current.pause()}>
+            <button className="bg-red-500 hover:bg-red-700 text-white py-2 px-4 rounded-full cursor-pointer" onClick={() => audioRef.current.pause()}>
                 Pause
             </button>
             <audio ref={audioRef}>
@@ -45,12 +45,12 @@ function App() {
             </audio>
         </section>
       <section>
-          <h1 className="text-2xl font-semibold mb-4 text-center">
+          <h1 className="text-2xl font-semibold mb-4 text-center custom">
               Songs:
           </h1>
           <ul className="flex flex-col space-y-2">
               {songs.map((song, index) => (
-                  <li key={song.id} onClick={() => setSelectedSong(index)}>
+                  <li key={song.id} onClick={() => setSelectedSong(index)} className={`text-center py-2 px-4 cursor-pointer rounded-lg ${songs[selectedSong].id === song.id ? 'bg-gray-700' : 'bg-gray-800'}`}>
                       {song.title} by {song.author}
                   </li>
               ))}
